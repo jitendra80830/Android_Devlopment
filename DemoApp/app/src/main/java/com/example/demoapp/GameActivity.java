61f8c57bc96296b5e8f5283dc50cf2c8a96b7882
jitendra <jitendrakumar89973@gmail.com>
2021-06-05 09:49:51 +0530

    android app

--- DemoApp/app/src/main/java/com/example/demoapp/GameActivity.java
+++ DemoApp/app/src/main/java/com/example/demoapp/GameActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -16,6 +18,12 @@ public class GameActivity extends AppCompatActivity {
     public  void playGameClicked(View view){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
+    }
+    public void playerTapped(View view){
+        ImageView imageView = (ImageView) view;
+        int tag = Integer.parseInt(imageView.getTag().toString());
+
+        Toast.makeText(getApplicationContext(),"Tapped" + tag,Toast.LENGTH_LONG).show();
     }
 
 }
