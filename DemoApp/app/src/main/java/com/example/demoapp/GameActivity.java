beb2d506196df351344484cc9b34fdadad26c50f
jitendra <jitendrakumar89973@gmail.com>
2021-06-08 17:10:50 +0530

    Android Apps

--- DemoApp/app/src/main/java/com/example/demoapp/GameActivity.java
+++ DemoApp/app/src/main/java/com/example/demoapp/GameActivity.java
@@ -1,19 +1,49 @@
 package com.example.demoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.prefs.PreferenceChangeEvent;
+
 public class GameActivity extends AppCompatActivity {
+
+    int playerTurn = 0;
+    TextView textView;
+
+    int gameState[] = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+    int allWinState[][] = {
+            {0,1,2} , {0,3,6} , {2,5,8} ,
+            {1,4,7} , {0,4,8}, {2,4,6},
+            {3,4,5},{6,7,8}
+    };
+
+    SharedPreferences sharedPreferences;
+    String player0 , player1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        player0 = sharedPreferences.getString("player1","first player");
+        player1 = sharedPreferences.getString("player2","second player");
+
+
+        textView = findViewById(R.id.textViewPlayerName);
+        textView.setText(player0+"'Turn");
+
     }
     public  void playGameClicked(View view){
         Intent intent = new Intent(this, HomeActivity.class);
@@ -23,7 +53,72 @@ public class GameActivity extends AppCompatActivity {
         ImageView imageView = (ImageView) view;
         int tag = Integer.parseInt(imageView.getTag().toString());
 
+
         Toast.makeText(getApplicationContext(),"Tapped" + tag,Toast.LENGTH_LONG).show();
+
+        //updating the game state array
+        gameState[tag] = playerTurn;
+
+
+        if(playerTurn == 0){
+            imageView.setImageResource(R.drawable.img_o);
+            playerTurn = 1;
+            textView.setText(player1+"'s Turn");
+        }else if(playerTurn == 1){
+            imageView.setImageResource(R.drawable.img_x);
+            playerTurn = 0;
+            textView.setText(player0+"'s Turn");
+
+
+        }
+
+        //checking the one of win state with matching game state array
+        for(int[] oneWinState : allWinState){
+            if(gameState[oneWinState[0]] != -1
+                    && gameState[oneWinState[0]] == gameState[oneWinState[1]]
+                    && gameState[oneWinState[1]] == gameState[oneWinState[2]]){
+
+                // checking who won
+                if(gameState[oneWinState[0]] == 0){
+                    // 0 wins
+                    textView.setText(player0+" wins");
+                    popupDialog(player0);
+                }else{
+                    textView.setText(player1+" wins");
+                    popupDialog(player1);
+                }
+
+
+            }
+
+        }
+    }
+
+    private void popupDialog(String playerName) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Congratulations You Win").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.create().show();;
+    }
+
+    public void resetGame(View view){
+        for(int i =0 ;i<gameState.length;i++){
+            gameState[i] = -1;
+        }
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+        textView.setText("Person Turn");
     }
 
 }
--- DemoApp/app/src/main/java/com/example/demoapp/PlayerNameActivity.java
+++ DemoApp/app/src/main/java/com/example/demoapp/PlayerNameActivity.java
@@ -3,22 +3,32 @@ package com.example.demoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class PlayerNameActivity extends AppCompatActivity {
+public class PlayerNameActivity<sharedPreferences> extends AppCompatActivity {
 
     EditText editTextPlayer1,editTextPlayer2;
+    SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_name);
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         editTextPlayer1 = findViewById(R.id.editTextTextPersonName);
         editTextPlayer2 = findViewById(R.id.editTextTextPersonName2);
+
+        //add the name which is stored in preference
+        editTextPlayer1.setText(sharedPreferences.getString("player1",""));
+        editTextPlayer2.setText(sharedPreferences.getString("player2",""));
 
     }
     public void startButtonClicked(View view){
@@ -28,6 +38,13 @@ public class PlayerNameActivity extends AppCompatActivity {
 
             Toast.makeText(this,"Please Enter Player's Name",Toast.LENGTH_LONG).show();
         }else {
+
+            //store the data of player in Shared preference
+            String player1 = editTextPlayer1.getText().toString();
+            String player2 = editTextPlayer2.getText().toString();
+
+            sharedPreferences.edit().putString("player1",player1).putString("player2",player2).apply();
+
             Intent intent = new Intent(this, GameActivity.class);
             startActivity(intent);
         }
