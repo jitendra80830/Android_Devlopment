ea91340a6c861d9cbb3b42dfa57bf6a7c160d678
jitendra <jitendrakumar89973@gmail.com>
2021-06-10 08:59:00 +0530

    Todo List App

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    TaskAdapter taskAdapter;
+    ImageView imageViewNewTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        populateTask();
+
+        imageViewNewTask = findViewById(R.id.imageViewNewTask);
+
+        imageViewNewTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void populateTask() {
+        ArrayList<Task> taskList = new ArrayList<Task>();
+        taskList.add(new Task("Task1","Description1"));
+        taskList.add(new Task("Task2","Description2"));
+        taskList.add(new Task("Task3","Description3"));
+        taskList.add(new Task("Task4","Description4"));
+        taskList.add(new Task("Task5","Description5"));
+        taskList.add(new Task("Task5","Description6"));
+
+        taskAdapter = new TaskAdapter(taskList,this);
+
+        recyclerView.setAdapter(taskAdapter);
     }
 }
--- TodoList/app/src/main/java/com/example/todolist/TaskAdapter.java
+++ TodoList/app/src/main/java/com/example/todolist/TaskAdapter.java
@@ -1,36 +1,58 @@
 package com.example.todolist;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskRowHolder> {
+
+    ArrayList<Task> taskList;
+    Context context;
+
+    public TaskAdapter(ArrayList<Task> taskList, Context context) {
+        this.taskList = taskList;
+        this.context = context;
+    }
 
     @NonNull
     @NotNull
     @Override
     public TaskRowHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        //taken an instance of row layout
+        View view = LayoutInflater.from(context).inflate(R.layout.row_holder,parent,false);
+        return new TaskRowHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull TaskAdapter.TaskRowHolder holder, int position) {
+        //binding the value to the TaskRowHolder class variable
+        holder.textViewTitle.setText(taskList.get(position).getTitle());
+        holder.textViewDesc.setText(taskList.get(position).getDescription());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return taskList.size();
     }
 
     public class TaskRowHolder extends RecyclerView.ViewHolder{
+        TextView textViewTitle,textViewDesc;
 
         public TaskRowHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
             super(itemView);
+            //attaching the textview to this class variable
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDesc = itemView.findViewById(R.id.textViewDesc);
         }
     }
 }
