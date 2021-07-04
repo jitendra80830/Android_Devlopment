354768dbea84a66f728f034b1cee9b4acbbe464b
jitendra <jitendrakumar89973@gmail.com>
2021-07-04 19:28:56 +0530

    Covid app

---
+++ Covid_/app/src/main/java/com/example/covid_/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.covid_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.covid_.api.ApiUtilities;
+import com.example.covid_.api.CountryData;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView totalConfirm,totalActive,totalRecovered,totalDeath,totalTest;
+    private TextView todayConfirm,todayRecovered,todayDeath,dateTv;
+
+    private List<CountryData> list;
+
+    private PieChart pieChartD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = new ArrayList<>();
+
+        init();
+
+        ApiUtilities.getApiInterface().getCountryData()
+                .enqueue(new Callback<List<CountryData>>() {
+                    @Override
+                    public void onResponse(Call<List<CountryData>> call, Response<List<CountryData>> response) {
+                        list.addAll(response.body());
+
+                        for(int i =0;i<list.size();i++){
+                            if(list.get(i).getCountry().equals("India")){
+                                int confirm = Integer.parseInt( list.get(i).getCases());
+                                int active = Integer.parseInt( list.get(i).getActive());
+                                int recovered = Integer.parseInt( list.get(i).getRecovered());
+                                int deaths = Integer.parseInt( list.get(i).getDeaths());
+
+                                totalConfirm.setText(NumberFormat.getInstance().format(confirm));
+
+                                totalActive.setText(NumberFormat.getInstance().format(active));
+                                totalRecovered.setText(NumberFormat.getInstance().format(recovered));
+                                totalDeath.setText(NumberFormat.getInstance().format(deaths));
+
+                                todayDeath.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTodayDeaths())));
+                                todayRecovered.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTodayRecovered())));
+                                todayConfirm.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTodayCases())));
+                                totalTest.setText(NumberFormat.getInstance().format(Integer.parseInt(list.get(i).getTests())));
+
+                                setText(list.get(i).getUpdated());
+
+                                pieChartD.addPieSlice(new PieModel("Confirm", confirm,getResources().getColor(R.color.yellow)));
+                                pieChartD.addPieSlice(new PieModel("Active", active,getResources().getColor(R.color.md_blue_700)));
+                                pieChartD.addPieSlice(new PieModel("Recovered", recovered,getResources().getColor(R.color.md_green_700)));
+                                pieChartD.addPieSlice(new PieModel("Deaths", deaths,getResources().getColor(R.color.md_red_700)));
+                                pieChartD.startAnimation();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CountryData>> call, Throwable t) {
+                        Toast.makeText(MainActivity.this,"Error : "+t.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+    private void setText(String updated) {
+        DateFormat format = new SimpleDateFormat("MMM, dd, yyyy");
+
+        Long milliSecond =  Long.parseLong(updated);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSecond);
+
+        dateTv.setText("Update at "+format.format(calendar.getTime()));
+    }
+
+    private void init(){
+        totalConfirm = (TextView)findViewById(R.id.totalConfirm);
+        todayConfirm= (TextView)findViewById(R.id.todayConfirm);
+        totalActive= (TextView)findViewById(R.id.totalActive);
+        totalRecovered= (TextView)findViewById(R.id.totalRecovered);
+        todayRecovered= (TextView)findViewById(R.id.todayRecovered);
+        totalDeath = (TextView)findViewById(R.id.totalDeath);
+        todayDeath= (TextView)findViewById(R.id.todayDeath);
+        totalTest= (TextView)findViewById(R.id.totalTest);
+        pieChartD= (PieChart)findViewById(R.id.pieChart);
+        dateTv = (TextView)findViewById(R.id.date);
+    }
+}
--- DemoList/app/src/main/java/com/example/demolist/CustomListViewActivity.java
+++ DemoList/app/src/main/java/com/example/demolist/CustomListViewActivity.java
@@ -10,30 +10,123 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class CustomListViewActivity extends AppCompatActivity {
 
     ListView listView;
-    String[] title  = {"Rahul","Ram","Jitendra","Rakesh","Akash","Ramesh","Rajesh","Ganesh"};
-    String[] desc  = {"Student","Actor","Student","Doctor","Businessman","Developer","Developer 1","Doctor"};
+    //String[] title  = {"Rahul","Ram","Jitendra","Rakesh","Akash","Ramesh","Rajesh","Ganesh"};
+    //String[] desc  = {"Student","Actor","Student","Doctor","Businessman","Developer","Developer 1","Doctor"};
 
-    int[] imageList = {R.drawable.heart,R.drawable.clipart,R.drawable.linkedin,R.drawable.wifi};
+    //int[] imageList = {R.drawable.heart,R.drawable.clipart,R.drawable.linkedin,R.drawable.wifi};
+    ArrayList<String> centerList,districtList;
+    ArrayList<Boolean> availableList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_list_view);
 
-        CustomAdapter arrayAdapter = new CustomAdapter();
+        centerList =  new ArrayList<String>();
+        districtList =  new ArrayList<String>();
+        availableList =  new ArrayList<Boolean>();
 
-        listView = findViewById(R.id.ListView);
-        listView.setAdapter(arrayAdapter);
+        String urlString = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin" +
+                "?pincode=826001&date=01-05-2021";
+
+        StringRequest request = new StringRequest(Request.Method.GET, urlString,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(CustomListViewActivity.this,"Url hit successfully",
+                                Toast.LENGTH_LONG).show();
+
+                        try {
+                            JSONObject parentObj = new JSONObject(response);
+                            JSONArray centers = parentObj.getJSONArray("centers");
+                            JSONObject jsonObject;
+                            String name,district;
+                            boolean isAvailable = false;
+                            JSONArray sessionArray;
+                            int available_capacity , min_age_limit;
+
+                            getSupportActionBar().setTitle("Centers Found : "+centers.length());
+
+                            for(int i = 0;i<centers.length();i++){
+                                jsonObject = centers.getJSONObject(i);
+                                 name  = jsonObject.getString("name");
+                                 district = jsonObject.getString("district_name");
+                                 isAvailable = false;
+
+
+
+                                sessionArray = jsonObject.getJSONArray("sessions");
+
+                                for(int j = 0;j<sessionArray.length();j++){
+                                    available_capacity = (sessionArray.getJSONObject(j))
+                                            .getInt("available_capacity");
+                                    min_age_limit = (sessionArray.getJSONObject(j))
+                                            .getInt("min_age_limit");
+
+                                    if(available_capacity > 0 && min_age_limit == 45){
+                                        district += " , "+available_capacity + " available";
+                                        isAvailable = true;
+
+                                    }
+
+                                }
+                                centerList.add(name);
+                                districtList.add(district);
+                                availableList.add(isAvailable);
+
+                            }
+
+                            CustomAdapter arrayAdapter = new CustomAdapter();
+
+                            listView = findViewById(R.id.ListView);
+                            listView.setAdapter(arrayAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(CustomListViewActivity.this,"Url hit Failed",
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+        RequestQueue requestQueue = Volley.newRequestQueue(CustomListViewActivity.this);
+        requestQueue.add(request);
+
+
+
     }
     public class CustomAdapter extends BaseAdapter{
 
         @Override
         public int getCount() {
-            return title.length;
+            return centerList.size();
         }
 
         @Override
@@ -51,13 +144,18 @@ public class CustomListViewActivity extends AppCompatActivity {
             convertView = getLayoutInflater().inflate(R.layout.custom_row_layout,null);
 
             TextView txtTitle = convertView.findViewById(R.id.textView);
-            txtTitle.setText(title[position]);
+            txtTitle.setText(centerList.get(position));
 
             TextView txtDesc = convertView.findViewById(R.id.textView2);
-            txtDesc.setText(desc[position]);
+            txtDesc.setText(districtList.get(position));
 
             ImageView imageView = convertView.findViewById(R.id.imageView);
-            imageView.setImageResource(imageList[position % imageList.length]);
+            if(availableList.get(position)){
+                imageView.setImageResource(R.drawable.vaccineicon);
+            }
+            else {
+                imageView.setImageResource(R.drawable.vacsin);
+            }
 
             return convertView;
         }
