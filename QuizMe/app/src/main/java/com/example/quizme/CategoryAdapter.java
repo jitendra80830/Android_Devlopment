6a6f206ddc59807ae4e4c1a420c499fc79edf236
jitendra <jitendrakumar89973@gmail.com>
2021-09-26 15:22:47 +0530

    Quiz App

--- QuizMe/app/src/main/java/com/example/quizme/CategoryAdapter.java
+++ QuizMe/app/src/main/java/com/example/quizme/CategoryAdapter.java
@@ -1,5 +1,7 @@
 package com.example.quizme;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,25 +10,43 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>{
+    Context context;
+    ArrayList<CategoryModel> categoryModels; // ArrayList
+
+    public CategoryAdapter(Context context, ArrayList<CategoryModel> categoryModels) {
+        this.context = context;
+        this.categoryModels = categoryModels;
+    }
 
     @NonNull
     @NotNull
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_catagory , null);
+        return new CategoryViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull CategoryAdapter.CategoryViewHolder holder, int position) {
+        CategoryModel model  = categoryModels.get(position);
+
+        holder.textView.setText(model.getCategoryName());
+        Glide.with(context)
+                .load(model.getCategoryImage())
+                .into(holder.imageView);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categoryModels.size();
     }
 
     public class CategoryViewHolder extends RecyclerView.ViewHolder{
--- QuizMe/app/src/main/java/com/example/quizme/MainActivity.java
+++ QuizMe/app/src/main/java/com/example/quizme/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.quizme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.quizme.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+
+        ArrayList<CategoryModel> categories = new ArrayList<>();
+
+        categories.add(new CategoryModel("","Mathematics","https://static.vecteezy.com/system/resources/previews/000/502/111/original/mathematics-icon-design-vector.jpg"));
+        categories.add(new CategoryModel("","Science","https://static.vecteezy.com/system/resources/previews/000/502/111/original/mathematics-icon-design-vector.jpg"));
+        categories.add(new CategoryModel("","History","https://static.vecteezy.com/system/resources/previews/000/502/111/original/mathematics-icon-design-vector.jpg"));
+        categories.add(new CategoryModel("","Languages","https://static.vecteezy.com/system/resources/previews/000/502/111/original/mathematics-icon-design-vector.jpg"));
+        categories.add(new CategoryModel("","Puzzle","https://static.vecteezy.com/system/resources/previews/000/502/111/original/mathematics-icon-design-vector.jpg"));
+        categories.add(new CategoryModel("","Drama","https://static.vecteezy.com/system/resources/previews/000/502/111/original/mathematics-icon-design-vector.jpg"));
+
+        CategoryAdapter adapter = new CategoryAdapter(this , categories);
+        binding.catogeryList.setLayoutManager(new GridLayoutManager(this , 2));
+        binding.catogeryList.setAdapter(adapter);
     }
 }
